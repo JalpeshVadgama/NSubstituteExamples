d718bd9a9c3dfa75da07d8a775224a79f9f5317a
Jalpesh Vadgama <jalpesh@dotnetjalps.com>
2015-03-13 02:08:21 +0530

    Initial Commit

---
+++ NSubstituteExamples/NSubstituteBasicDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Linq;
+
+namespace NSubstituteBasicDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ NSubstituteExamples/NSubstituteTest/PersonTest.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NSubstitute;
+using NSubstituteBasicDemo;
+
+namespace NSubstituteTest
+{
+    [TestClass]
+    public class PersonTest
+    {
+        [TestMethod]
+        public void ToCheck_Whether_Person_FullName_Work_Or_Not()
+        {
+            //Creating Substiute for person
+            var person = Substitute.For<IPerson>();
+            
+            //Adding a return value
+            person.GetFullName("Jalpesh", "Vadgama").Returns("Jalpesh Vadgama");
+
+            //Asserting to check whether its fine or not
+            Assert.AreEqual("Jalpesh Vadgama", person.GetFullName("Jalpesh", "Vadgama"));
+
+        }
+    }
+}
